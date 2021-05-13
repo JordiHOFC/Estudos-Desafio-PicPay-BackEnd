b3ade696225049601538701f825b8f1b45b3e57d
jordihofc <jordi.silva@ufv.br>
2021-05-13 19:54:28 -0300

    setup projeto

---
+++ src/main/java/br/com/desafiobackend/picpay/transacoes/TransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiobackend.picpay.transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiobackend/picpay/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiobackend.picpay.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
